797eb00ac70d13e9f9fd16c8c3bbe553de45667b
antony-akshay <akshay.dev.ac.in@gmail.com>
2026-02-09 14:04:14 +0530

    tree-inorder

---
+++ leetcode/tree/inorder.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def bfs(root):
+            if root is None: return
+            bfs(root.left)
+            res.append(root.val)
+            bfs(root.right)
+        bfs(root)
+        return res
